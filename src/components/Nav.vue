9cdac90608368a93546531980494c0d619dad27c
User <sdg0627@naver.com>
2023-06-05 09:47:27 +0900

    1

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
-
-  <div class="w-full px-[2%] sticky top-0 bg-white/50 py-2.5 z-50 ">
+  <div>
+  <div class="w-full px-[2%] fixed top-0 bg-white/50 py-2.5 z-50 ">
     <div class="max-w-7xl mx-auto flex justify-between items-center ">
         <div>
           <button @click="$emit('MenuIndex','메인페이지')">
@@ -33,7 +33,7 @@
         
     </div>
   </div>
-  
+</div>
 </template>
 
 <script>
